9dec64b548ff50979f7832384d39c50ba47a2bac
akbar112 <75440379+akbar112@users.noreply.github.com>
2023-09-30 13:22:06 +0700

    Add files via upload

--- spk_saw.sql
+++ spk_saw.sql
@@ -1,98 +1,161 @@
 -- phpMyAdmin SQL Dump
--- version 4.3.11
--- http://www.phpmyadmin.net
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: 24 Mar 2016 pada 08.58
--- Versi Server: 5.6.24
--- PHP Version: 5.6.8
+-- Generation Time: Sep 29, 2023 at 09:18 AM
+-- Server version: 10.4.22-MariaDB
+-- PHP Version: 8.1.1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Database: `spk_saw`
 --
-CREATE DATABASE IF NOT EXISTS `spk_saw` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
-USE `spk_saw`;
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `alternatif`
+-- Table structure for table `alternatif`
 --
 
-CREATE TABLE IF NOT EXISTS `alternatif` (
+CREATE TABLE `alternatif` (
   `id_alternatif` int(11) NOT NULL,
   `nama_alternatif` varchar(255) NOT NULL,
   `hasil_alternatif` double NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `alternatif`
+--
+
+INSERT INTO `alternatif` (`id_alternatif`, `nama_alternatif`, `hasil_alternatif`) VALUES
+(13, 'BMC', 11.45),
+(14, 'KYT ', 14.666666666667),
+(15, 'NJS', 14.1),
+(16, 'CARGLOSS', 14.416666666667),
+(17, 'NHK', 13.75);
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `kriteria`
+-- Table structure for table `kriteria`
 --
 
-CREATE TABLE IF NOT EXISTS `kriteria` (
+CREATE TABLE `kriteria` (
   `id_kriteria` int(11) NOT NULL,
   `nama_kriteria` varchar(255) NOT NULL,
   `tipe_kriteria` varchar(10) NOT NULL,
   `bobot_kriteria` double NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `kriteria`
+--
+
+INSERT INTO `kriteria` (`id_kriteria`, `nama_kriteria`, `tipe_kriteria`, `bobot_kriteria`) VALUES
+(13, 'Harga', 'cost', 4),
+(14, 'Berat', 'benefit', 3),
+(15, 'Double Visor', 'benefit', 3),
+(16, 'Sertifikasi ( SNI / SNI DOT )', 'benefit', 5),
+(17, 'Garansi', 'benefit', 3);
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `nilai`
+-- Table structure for table `nilai`
 --
 
-CREATE TABLE IF NOT EXISTS `nilai` (
+CREATE TABLE `nilai` (
   `id_nilai` int(6) NOT NULL,
   `ket_nilai` varchar(45) NOT NULL,
   `jum_nilai` double NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `nilai`
+--
+
+INSERT INTO `nilai` (`id_nilai`, `ket_nilai`, `jum_nilai`) VALUES
+(13, 'Sangat Penting', 5),
+(14, 'Penting', 4),
+(15, 'Cukup Penting', 3),
+(16, 'Kurang Penting', 2),
+(17, 'Tidak penting', 1);
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `pengguna`
+-- Table structure for table `pengguna`
 --
 
-CREATE TABLE IF NOT EXISTS `pengguna` (
+CREATE TABLE `pengguna` (
   `id_pengguna` int(11) NOT NULL,
   `nama_lengkap` varchar(255) NOT NULL,
   `username` varchar(100) NOT NULL,
   `password` varchar(100) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data untuk tabel `pengguna`
+-- Dumping data for table `pengguna`
 --
 
 INSERT INTO `pengguna` (`id_pengguna`, `nama_lengkap`, `username`, `password`) VALUES
-(1, 'Komarudin Akbar', 'admin', 'd73f53718877dc21fa7bc9056214d485'),
-(2, 'Abay', 'user', 'd86abf4f734abebf036832a8bc51fdfa');
+(1, 'Akbar', 'admin', 'd73f53718877dc21fa7bc9056214d485'),
+(2, 'Code Berkas', 'user', 'ee11cbb19052e40b07aac0ca060c23ee');
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `rangking`
+-- Table structure for table `rangking`
 --
 
-CREATE TABLE IF NOT EXISTS `rangking` (
+CREATE TABLE `rangking` (
   `id_alternatif` int(11) NOT NULL,
   `id_kriteria` int(11) NOT NULL,
   `nilai_rangking` double NOT NULL,
   `nilai_normalisasi` double NOT NULL,
   `bobot_normalisasi` double NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `rangking`
+--
+
+INSERT INTO `rangking` (`id_alternatif`, `id_kriteria`, `nilai_rangking`, `nilai_normalisasi`, `bobot_normalisasi`) VALUES
+(13, 13, 2, 1, 4),
+(13, 14, 2, 0.5, 1.5),
+(13, 15, 3, 0.75, 2.25),
+(13, 16, 2, 0.5, 2.5),
+(13, 17, 2, 0.4, 1.2),
+(14, 13, 3, 0.66666666666667, 2.6666666666667),
+(14, 14, 3, 0.75, 2.25),
+(14, 15, 4, 1, 3),
+(14, 16, 3, 0.75, 3.75),
+(14, 17, 5, 1, 3),
+(15, 13, 5, 0.4, 1.6),
+(15, 14, 3, 0.75, 2.25),
+(15, 15, 3, 0.75, 2.25),
+(15, 16, 4, 1, 5),
+(15, 17, 5, 1, 3),
+(16, 13, 3, 0.66666666666667, 2.6666666666667),
+(16, 14, 3, 0.75, 2.25),
+(16, 15, 2, 0.5, 1.5),
+(16, 16, 4, 1, 5),
+(16, 17, 5, 1, 3),
+(17, 13, 5, 0.4, 1.6),
+(17, 14, 4, 1, 3),
+(17, 15, 4, 1, 3),
+(17, 16, 3, 0.75, 3.75),
+(17, 17, 4, 0.8, 2.4);
 
 --
 -- Indexes for dumped tables
@@ -126,7 +189,8 @@ ALTER TABLE `pengguna`
 -- Indexes for table `rangking`
 --
 ALTER TABLE `rangking`
-  ADD PRIMARY KEY (`id_alternatif`,`id_kriteria`), ADD KEY `id_kriteria` (`id_kriteria`);
+  ADD PRIMARY KEY (`id_alternatif`,`id_kriteria`),
+  ADD KEY `id_kriteria` (`id_kriteria`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -136,32 +200,37 @@ ALTER TABLE `rangking`
 -- AUTO_INCREMENT for table `alternatif`
 --
 ALTER TABLE `alternatif`
-  MODIFY `id_alternatif` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=13;
+  MODIFY `id_alternatif` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+
 --
 -- AUTO_INCREMENT for table `kriteria`
 --
 ALTER TABLE `kriteria`
-  MODIFY `id_kriteria` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=13;
+  MODIFY `id_kriteria` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+
 --
 -- AUTO_INCREMENT for table `nilai`
 --
 ALTER TABLE `nilai`
-  MODIFY `id_nilai` int(6) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=13;
+  MODIFY `id_nilai` int(6) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+
 --
 -- AUTO_INCREMENT for table `pengguna`
 --
 ALTER TABLE `pengguna`
-  MODIFY `id_pengguna` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
+  MODIFY `id_pengguna` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
 --
--- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
+-- Constraints for dumped tables
 --
 
 --
--- Ketidakleluasaan untuk tabel `rangking`
+-- Constraints for table `rangking`
 --
 ALTER TABLE `rangking`
-ADD CONSTRAINT `rangking_ibfk_1` FOREIGN KEY (`id_alternatif`) REFERENCES `alternatif` (`id_alternatif`),
-ADD CONSTRAINT `rangking_ibfk_2` FOREIGN KEY (`id_kriteria`) REFERENCES `kriteria` (`id_kriteria`);
+  ADD CONSTRAINT `rangking_ibfk_1` FOREIGN KEY (`id_alternatif`) REFERENCES `alternatif` (`id_alternatif`),
+  ADD CONSTRAINT `rangking_ibfk_2` FOREIGN KEY (`id_kriteria`) REFERENCES `kriteria` (`id_kriteria`);
+COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
